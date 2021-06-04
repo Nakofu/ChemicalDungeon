1ea8f14833f852f0f8a98c942c288c541a1715d3
Sychqh <sychqh@gmail.com>
2021-06-04 15:34:19 +0500

    Фикс мерж

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,20 +7,43 @@ public class Enemy : MonoBehaviour
     [SerializeField] private SpriteRenderer spr;
     [SerializeField] private GameObject text;
     [SerializeField] private Substance substance;
+    [SerializeField] private Transform playerPos;
+    [SerializeField] private Transform tr;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed;
 
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 movement;
+
+    private void Start()
     {
         spr = GetComponent<SpriteRenderer>();
         text = transform.GetChild(0).gameObject;
         substance = GetComponent<Substance>();
         text.GetComponent<TextMesh>().text = substance.Formula;
         spr.color = substance.Color;
+        playerPos = GameObject.Find("Player").GetComponent<Transform>();
+        tr = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+        movement = new Vector2(0, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (playerPos.position.x > tr.position.x)
+            movement.x++;
+        if (playerPos.position.x < tr.position.x)
+            movement.x--;
+        if (playerPos.position.y > tr.position.y)
+            movement.y++;
+        if (playerPos.position.y < tr.position.y)
+            movement.y--;
+
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+        movement = new Vector2(0, 0);
     }
 }
