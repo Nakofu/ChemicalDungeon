55e1196672e19c21f30c035420987734b55af24c
Sychqh <sychqh@gmail.com>
2021-06-09 13:02:23 +0500

    Добавил стрельбу с новыми спрайтами

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,24 +6,54 @@ public class Gun : MonoBehaviour
 {
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private float bulletForce;
+    private Vector3 shootVector;
+    private Quaternion bulletRot;
     public Substance Substance;
 
     public void Shoot()
     {
-        var bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        var bullet = Instantiate(bulletPrefab, transform.position, bulletRot);// transform.rotation);
         bullet.GetComponent<Bullet>().Substance = Substance;
-        bullet.GetComponent<Rigidbody2D>().AddForce(-transform.right * bulletForce, ForceMode2D.Impulse);
+        bullet.GetComponent<Rigidbody2D>().AddForce(shootVector * bulletForce, ForceMode2D.Impulse);
     }
 
     private void Start()
     {
-        bulletForce = 10f;
         bulletPrefab = Resources.Load<GameObject>("bullet");
     }
 
 
     void Update()
     {
-        
+        var playerMov = transform.GetComponentInParent<PlayerControl>().movement;
+        var playerPos = transform.parent.transform.position;
+
+        if (playerMov.x == 1 && playerMov.y == 0)
+        {
+            transform.position = new Vector3(playerPos.x + 1.1f, playerPos.y - 0.1f);
+            bulletRot = Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f));
+            shootVector = transform.right;
+        }
+
+        if (playerMov.x == -1 && playerMov.y == 0)
+        {
+            transform.position = new Vector3(playerPos.x - 1.1f, playerPos.y - 0.1f);
+            bulletRot = Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f));
+            shootVector = -transform.right;
+        }
+
+        if (playerMov.x == 0 && playerMov.y == 1)
+        {
+            transform.position = new Vector3(playerPos.x + 0.3f, playerPos.y + 1.3f);
+            bulletRot = Quaternion.Euler(new Vector3(0.0f, 0.0f, 90.0f));
+            shootVector = transform.up;
+        }
+
+        if (playerMov.x == 0 && (playerMov.y == -1 || playerMov.y == 0))
+        {
+            transform.position = new Vector3(playerPos.x - 0.0f, playerPos.y - 0.9f);
+            bulletRot = Quaternion.Euler(new Vector3(0.0f, 0.0f, 90.0f));
+            shootVector = -transform.up;
+        }
     }
 }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,17 +7,13 @@ public class PlayerControl : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Camera cam;
     [SerializeField] private Animator animator;
 
-    private Vector2 movement;
-    private Vector2 mousePos;
+    public Vector2 movement;
 
     private void Start()
     {
-        moveSpeed = 10f;
         rb = GetComponent<Rigidbody2D>();
-        cam = FindObjectOfType<Camera>();
         animator = GetComponent<Animator>();
     }
 
@@ -30,8 +26,6 @@ public class PlayerControl : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
-
-        //mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
         if (Input.GetButtonDown("Fire1") && !PauseMenu.GameIsPaused)
         {
@@ -47,9 +41,5 @@ public class PlayerControl : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
-
-        //Vector2 lookDir = mousePos - rb.position;
-        //float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
-        //rb.rotation = angle;
     }
 }
